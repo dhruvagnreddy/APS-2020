17091736230a2f134228bca5f50e8b56a6b79807
dhruvagnreddy <51833620+dhruvagnreddy@users.noreply.github.com>
2020-05-15 20:05:29 +0530

    Create Modified_Kaprekar_Numbers.py

---
+++ Modified_Kaprekar_Numbers.py
@@ -0,0 +1,16 @@
+
+p=int(input())
+q=int(input())
+flag = True
+for i in range(p,q+1):
+    sq = i*i
+    s=str(sq)
+    if sq < 10:
+        s = '0'+s
+    l = s[0:int(len(s)/2)]
+    r = s[int(len(s)/2):]
+    if int(l)+int(r) == i:
+        print (i , end = ' ')
+        flag = False
+if flag:
+    print ("INVALID RANGE")
